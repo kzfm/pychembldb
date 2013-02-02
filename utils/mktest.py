09ead55cf58398ecfe318af46b9e86ecda5450e5
Kazufumi Ohkawa <kerolinq@gmail.com>
2013-02-03 07:36:48 +0900

    Add testcase generator

--- utils/mktest.py
+++ utils/mktest.py
@@ -1,9 +1,23 @@
+from singular import singularize
 from sqlalchemy import create_engine, MetaData
 import sys
-db = create_engine('mysql://root@localhost/chembl_14')
-metadata = MetaData(bind=db, reflect=True)
+db = create_engine('mysql://root@localhost/chembl_15')
+metadata = MetaData(bind=db)
+metadata.reflect()
 
-table = metadata.tables[sys.argv[1]]
+tablename = sys.argv[1]
+classname = "".join([singularize(n).capitalize() for n in tablename.split('_') if n not in ['dictionary', 'lookup']])
+testname = classname + 'Test'
+table = metadata.tables[tablename]
+
+print """import unittest
+from pychembldb import chembldb, {}
+
+
+class {}(unittest.TestCase):
+    def setUp(self):
+        self.target = chembldb.query(Target).get(1)
+""".format(classname, testname)
 
 for c in table.columns:
     print """    def test_{}(self):
