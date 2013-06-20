80552376af1072edfab54c9ffbeffd122652d2b3
Kazufumi Ohkawa <kerolinq@gmail.com>
2013-06-20 09:36:30 +0900

    Add inchikey search api

--- examples/chemblapi/app.py
+++ examples/chemblapi/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from pychembldb import chembldb, Molecule
+from pychembldb import chembldb, Molecule, CompoundStructure
 app = Flask(__name__)
 app.debug = True
 
@@ -36,7 +36,6 @@ def is_ro3_pass(c):
 @app.route("/chemblws/compounds/<chembl_id>")
 def compound_by_ChEMBLID(chembl_id):
     chembl_id, format = check_query(chembl_id)
-    print chembl_id
     compound = chembldb.query(Molecule).filter_by(chembl_id=chembl_id).one()
 
     result = {
@@ -45,6 +44,32 @@ def compound_by_ChEMBLID(chembl_id):
         "acdLogp": float(compound.property.acd_logp),
         "alogp": float(compound.property.alogp),
         "chemblId": chembl_id,
+        "knownDrug": is_known_drug(compound),
+        "medChemFriendly": is_med_chem_friendly(compound),
+        "molecularFormula": compound.structure.molformula,
+        "molecularWeight": float(compound.property.full_mwt),
+        "numRo5Violations": compound.property.num_ro5_violations,
+        "passesRuleOfThree": is_ro3_pass(compound),
+        "rotatableBonds": compound.property.rtb,
+        "smiles": compound.structure.canonical_smiles,
+        "stdInChiKey": compound.structure.standard_inchi_key
+        }
+    }
+    return jsonify(result)
+
+
+@app.route("/chemblws/compounds/stdinchikey/<inchikey>")
+def compound_by_InCHIKEY(inchikey):
+    inchikey, format = check_query(inchikey)
+    structure = chembldb.query(CompoundStructure).filter_by(standard_inchi_key=inchikey).one()
+    compound = structure.molecule
+
+    result = {
+    "compound": {
+        "acdLogd": float(compound.property.acd_logd),
+        "acdLogp": float(compound.property.acd_logp),
+        "alogp": float(compound.property.alogp),
+        "chemblId": compound.chembl_id,
         "knownDrug": is_known_drug(compound),
         "medChemFriendly": is_med_chem_friendly(compound),
         "molecularFormula": compound.structure.molformula,
